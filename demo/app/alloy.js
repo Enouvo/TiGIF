84f9be93cfcca9b6e08c7e7d3401c73386ba221f
Duy Bao Nguyen <duy.nguyen@enouvo.com>
2015-04-03 11:29:05 +1100

    test demo project

--- demo/app/alloy.js
+++ demo/app/alloy.js
@@ -1,11 +1,5 @@
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = function(){};
+var test = require('com.enouvo.tigif');
+Ti.API.info("module is => " + test);
+Ti.API.info("module example() method returns => " + test.example());
+Ti.API.info("module exampleProp is => " + test.exampleProp);
+test.exampleProp = "This is a test value";
