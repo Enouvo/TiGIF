98059422152ed4528217c14a8f12bd0304199464
Duy Bao Nguyen <duy.nguyen@enouvo.com>
2015-04-05 17:09:53 +1000

    updat demo

--- demo/app/controllers/index.js
+++ demo/app/controllers/index.js
@@ -1,3 +1,8 @@
+$.image.center = {
+	x : Titanium.Platform.displayCaps.platformWidth / 2,
+	y : Titanium.Platform.displayCaps.platformHeight / 2
+};
+
 $.image.gif = "http://raphaelschaad.com/static/nyan.gif";
 
 $.index.open();
